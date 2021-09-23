4621f867e994826f751bb5eb87d60cdd3d0fabf6
Jordan Hart <p22jhart@fairfieldprep.org>
2021-09-23 08:03:19 -0700

    Lab

---
+++ lab_5-1.py
@@ -0,0 +1,29 @@
+x = 2
+
+y = 4
+
+w = 5
+
+t = 7
+
+a = 6
+
+i = 9
+
+z = 10
+
+
+
+print(x = x+ 5)
+
+print(y -= 3)
+
+print (z **= 2)
+
+print (w = w % 2)
+
+print (t = t / 5)
+
+print (a *= 12)
+
+print (i = i // 7)
